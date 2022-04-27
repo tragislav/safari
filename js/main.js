b8d9c45b8e83c8e59e5ca100976f1a0082dfa2b0
Stanislau Dykomenko <stanislau.dykomenko@effective-soft.com>
2022-04-27 15:04:19 +0300

    gallery

--- js/main.js
+++ js/main.js
@@ -8,4 +8,23 @@ $(function () {
 
     fixedContentPos: false,
   });
+
+  $(".gallery__slider").slick({
+    prevArrow:
+      '<button type="button" class="slick-btn slick-prev"><img src="images/arrow-left.svg"/></button>',
+    nextArrow:
+      '<button type="button" class="slick-btn slick-next"><img src="images/arrow-right.svg"/></button>',
+  });
+
+  $(".gallery__item-inner").magnificPopup({
+    delegate: "a",
+    type: "image",
+    tLoading: "Loading image #%curr%...",
+    mainClass: "mfp-img-mobile",
+    gallery: {
+      enabled: true,
+      navigateByImgClick: true,
+      preload: [0, 1], // Will preload 0 - before current, and 1 after the current image
+    },
+  });
 });
